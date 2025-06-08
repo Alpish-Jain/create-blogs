15380eee206bc05fff178bfec0487ac1735f7118
Alpish-Jain <46166671+Alpish-Jain@users.noreply.github.com>
2025-06-08 13:36:25 +0530

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    //TODO: add a post:postSlice
+  },
+});
+
+export default store;
